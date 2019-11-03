4bd29e931fe896cc6cbf32af32baf85a1c6e1f50
atoris1192 <atoris1192@gmail.com>
2019-11-04 08:55:27 +0900

    add

---
+++ app/src/store/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const todo = {
+  namespaced: true,
+  state: {
+    todos: [
+      { id: 0, title: 'title0', body: 'body0', isDone: false },
+      { id: 1, title: 'title1', body: 'body1', isDone: true },
+      { id: 2, title: 'title2', body: 'body2', isDone: false },
+    ],
+  },
+  mutations: {
+    addTodo(state, item) {
+      state.push(item);
+    }
+  },
+  actions: {
+    addTodo(item) {
+      this.commit('todo/addTodo', item)
+    }
+  },
+  getters: {
+    getTodos(state) {
+      return state.todos;
+    },
+    getTodoCount(state) {
+      return state.todos.length;
+    }
+  }
+}
+
+
+export default new Vuex.Store({
+  modules: {
+    todo,
+  },
+});
